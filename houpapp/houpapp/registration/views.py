417df92d1755d66f877fcc18d35cbf027e8b768a
Fernando <flcavqueiroz@gmail.com>
2020-03-16 13:46:46 -0300

    Cria página de cadastro

--- houpapp/houpapp/registration/views.py
+++ houpapp/houpapp/registration/views.py
@@ -49,7 +49,6 @@ def detail(request, pk):
                     {'registration': registration   })
 
 
-
 def _send_mail(subject, from_, to, template_name, context):
     body = render_to_string(template_name, context)
     mail.send_mail(subject, body, from_, [from_, to])
