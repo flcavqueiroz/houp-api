7666897eeb6a4b201f88c2ec98de7c02d4129618
Fernando <flcavqueiroz@gmail.com>
2020-04-22 12:37:35 -0300

    refaz hierarquia dos arquivos

--- houpapp/houpapp/registration/admin.py
+++ houpapp/houpapp/registration/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from houpapp.registration.models import Registration, Login
+from houpapp.houpapp.registration.models import Registration, Login
 from django.utils.timezone import now
 
 class RegistrationModelAdmin(admin.ModelAdmin):
